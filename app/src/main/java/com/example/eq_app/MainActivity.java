d317fe450e21f3254fd3c3b9af92f9d19663b998
akaybanez <akaybanez@gmail.com>
2019-09-26 01:09:33 +0800

    added menu button onclick

--- app/src/main/java/com/example/eq_app/MainActivity.java
+++ app/src/main/java/com/example/eq_app/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn_instructions, btn_map, btn_guide;
