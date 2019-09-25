3ca720f9cce792c1aae4174fa7df30a998f2cfd6
akaybanez <akaybanez@gmail.com>
2019-09-26 00:09:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/eq_app/GuideActivity.java
@@ -0,0 +1,18 @@
+package com.example.eq_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class GuideActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+
+    }
+}
---
+++ app/src/main/java/com/example/eq_app/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.eq_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn_instructions, btn_map, btn_guide;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_instructions = findViewById(R.id.instructionsBtn);
+        btn_map = findViewById(R.id.mapBtn);
+        btn_guide = findViewById(R.id.guideBtn);
+
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MapActivity.class);
+                view.getContext().startActivity(intent);}
+        });
+
+        btn_guide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), GuideActivity.class);
+                view.getContext().startActivity(intent);}
+        });
+
+    }
+}
