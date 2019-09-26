235d0b4f74501114462e944d7f5dc8f87010191e
akaybanez <akaybanez@gmail.com>
2019-09-26 14:43:20 +0800

    working guide activity

--- app/src/main/java/com/example/eq_app/GuideActivity.java
+++ app/src/main/java/com/example/eq_app/GuideActivity.java
@@ -3,16 +3,72 @@ package com.example.eq_app;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class GuideActivity extends AppCompatActivity {
+    private TextView[] mDots;
+    private ViewPager vPager;
+    private ImageAdapter imageAdapter;
+    private LinearLayout mDotLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
 
+        Toast.makeText(this, "Swipe left!", Toast.LENGTH_SHORT).show();
+
+        vPager = findViewById(R.id.viewPageAndroid);
+        mDotLayout = findViewById(R.id.dotsLayout);
+
+        imageAdapter = new ImageAdapter(this);
+        vPager.setAdapter(imageAdapter);
+
+        addDotsIndicator(0);
+        vPager.addOnPageChangeListener(viewListener);
     }
+
+    public void addDotsIndicator(int position){
+
+        mDots = new TextView[3];
+        mDotLayout.removeAllViews();
+
+        for(int i = 0; i < mDots.length; i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("•"));
+            mDots[i].setTextSize(45);
+            mDots[i].setTextColor(getResources().getColor(R.color.transparentWhite));
+
+            mDotLayout.addView(mDots[i]);
+        }
+
+        if(mDots.length > 0){
+            mDots[position].setTextColor(getResources().getColor(R.color.white));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            addDotsIndicator(i);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+        }
+    };
+
 }
---
+++ app/src/main/java/com/example/eq_app/ImageAdapter.java
@@ -0,0 +1,45 @@
+package com.example.eq_app;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+public class ImageAdapter extends PagerAdapter {
+    Context mContext;
+
+    ImageAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return sliderImagesId.length;
+    }
+
+    private int[] sliderImagesId = new int[]{
+            R.drawable.guide1, R.drawable.guide2, R.drawable.guide3,
+    };
+
+    @Override
+    public boolean isViewFromObject(View v, Object obj) {
+        return v == ((ImageView) obj);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int i) {
+        ImageView mImageView = new ImageView(mContext);
+        mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        mImageView.setImageResource(sliderImagesId[i]);
+        ((ViewPager) container).addView(mImageView, 0);
+        return mImageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int i, Object obj) {
+        ((ViewPager) container).removeView((ImageView) obj);
+    }
+}
